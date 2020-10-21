6be7c530443519c6c6404239758d6818512917c8
John Uhri <john.uhri@redbitbluebit.com>
2020-10-21 18:53:38 -0500

    Added App.js // hello, world!

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello, World!</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.querySelector('#main'));
