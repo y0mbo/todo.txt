512362760166961c7462c49d9a722bbebf280f50
John Uhri <john.uhri@redbitbluebit.com>
2021-03-12 16:23:33 -0600

    Renamed from List to Tasks

---
+++ src/components/Tasks.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Tasks extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>My todo list</h2>
+
+        <ul></ul>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Tasks;
