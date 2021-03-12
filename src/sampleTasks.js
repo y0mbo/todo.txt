8a7b009a2b966c97702fee8561df8f1cb899ec8a
John Uhri <john.uhri@redbitbluebit.com>
2021-03-12 17:01:06 -0600

    Created sampleTasks.js

---
+++ src/sampleTasks.js
@@ -0,0 +1,21 @@
+// sample data to get things started
+
+const tasks = {
+  task1: {
+    priority: "A",
+    createDate: "2021-03-12",
+    description: "Create sample data.",
+    raw: "(A) 2021-03-12 Create sample data.",
+  },
+  task2: {
+    priority: "B",
+    createDate: "2021-03-12",
+    description: "Another task with a +project",
+    raw: "(B) 2021-03-12 Another task with a +project",
+  },
+  task3: {
+    createDate: "2021-03-12",
+    description: "A task with @context",
+    raw: "2021-03-12 A task with @context",
+  },
+};
