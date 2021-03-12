d754ac4ab2cd69b4529a80b496e6f6286f92daa9
John Uhri <john.uhri@redbitbluebit.com>
2021-03-12 16:23:22 -0600

    Added state and renamed the List object to Tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Tasks from "./Tasks";
 
 class App extends React.Component {
+  state = {
+    tasks: {},
+    priorities: {},
+    projects: {},
+  };
+
   render() {
     return (
       <React.Fragment>
         <h1>Hello, World!</h1>
+        <p>This is John's todo.txt implementation</p>
+        <Tasks tasks={this.state.tasks}></Tasks>
       </React.Fragment>
     );
   }
