e508967ad3ebe3be784ac19cea7f5afd905d57dd
John Uhri <john.uhri@redbitbluebit.com>
2021-03-23 21:24:27 -0500

    Styled components a little.

--- src/components/Contexts.js
+++ src/components/Contexts.js
@@ -1,4 +1,31 @@
 import React from "react";
+import css from "styled-components";
+
+const TagStyle = css.div`
+
+ background-color: #DCE3F7;
+ border: 1px solid #355EAE;
+ color: #355EAE;
+ display: inline-block;
+ font-family: arial;
+ font-size: 12px;
+ height: 20px;
+ border-radius: 8px;
+ margin-bottom: 20px;
+ padding-left: 8px;
+
+`;
+
+const TagCount = css.span`
+color: #DCE3F7;
+background-color: #355EAE;
+height: 100%;
+border-radius: 6px;
+margin-left: 8px;
+width: 20px;
+display: inline-block;
+text-align: center;
+`;
 
 class Contexts extends React.Component {
   render() {
@@ -7,7 +34,9 @@ class Contexts extends React.Component {
         <ul>
           {Object.keys(this.props.contexts).map((key) => (
             <li key={key}>
-              {key} {this.props.contexts[key]}
+              <TagStyle>
+                {key} <TagCount>{this.props.contexts[key]}</TagCount>
+              </TagStyle>
             </li>
           ))}
         </ul>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,30 @@
 import React from "react";
+import css from "styled-components";
+
+const TagStyle = css.div`
+ background-color: #DCF7E3;
+ border: 1px solid #35AE5E;
+ color: #35AE5E;
+ display: inline-block;
+ font-family: arial;
+ font-size: 12px;
+ height: 20px;
+ border-radius: 8px;
+ margin-bottom: 20px;
+ padding-left: 8px;
+`;
+
+const TagCount = css.span`
+color: #DCF7E3;
+background-color: #35AE5E;
+display: inline;
+height: 100%;
+border-radius: 6px;
+margin-left: 8px;
+width: 20px;
+display: inline-block;
+text-align: center;
+`;
 
 class Projects extends React.Component {
   render() {
@@ -7,7 +33,9 @@ class Projects extends React.Component {
         <ul>
           {Object.keys(this.props.projects).map((key) => (
             <li key={key}>
-              {key} {this.props.projects[key]}
+              <TagStyle>
+                {key} <TagCount>{this.props.projects[key]}</TagCount>
+              </TagStyle>
             </li>
           ))}
         </ul>
