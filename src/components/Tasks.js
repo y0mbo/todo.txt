57c960d2264437e7c48c6778122cf4ec5e204908
John Uhri <john.uhri@redbitbluebit.com>
2021-03-30 21:48:48 -0500

    Added styled component instead of ul

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Task from "./Task";
+import css from "styled-components";
+
+const TaskList = css.ul`
+  list-style: none;
+  margin: 0;
+	padding: 0;
+`;
 
 class Tasks extends React.Component {
   render() {
     return (
       <div>
         <h2>My todo list</h2>
-        <ul className="tasks">
+        <TaskList>
           {/* loop through state.tasks' keys and map that. */}
           {Object.keys(this.props.tasks).map((key) => (
             <Task key={key} id={key} details={this.props.tasks[key]} />
           ))}
-        </ul>
+        </TaskList>
         <span>{Object.keys(this.props.tasks).length} tasks</span>
       </div>
     );
