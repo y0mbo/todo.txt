476a44254a2e22e2a068ed6a1e8658df50dff081
John Uhri <john.uhri@redbitbluebit.com>
2021-03-30 21:48:27 -0500

    Cleared out some css.

--- src/components/Contexts.js
+++ src/components/Contexts.js
@@ -1,45 +1,23 @@
 import React from "react";
 import css from "styled-components";
 
-const TagStyle = css.div`
-
- background-color: #DCE3F7;
- border: 1px solid #355EAE;
- color: #355EAE;
- display: inline-block;
- font-family: arial;
- font-size: 12px;
- height: 20px;
- border-radius: 8px;
- margin-bottom: 20px;
- padding-left: 8px;
-
-`;
-
-const TagCount = css.span`
-color: #DCE3F7;
-background-color: #355EAE;
-height: 100%;
-border-radius: 6px;
-margin-left: 8px;
-width: 20px;
-display: inline-block;
-text-align: center;
+const Tags = css.ul`
+  margin: 1em 0 0;
+	padding: 0;
+	list-style: none;
 `;
 
 class Contexts extends React.Component {
   render() {
     return (
       <div>
-        <ul>
+        <Tags>
           {Object.keys(this.props.contexts).map((key) => (
             <li key={key}>
-              <TagStyle>
-                {key} <TagCount>{this.props.contexts[key]}</TagCount>
-              </TagStyle>
+              {key} <span>{this.props.contexts[key]}</span>
             </li>
           ))}
-        </ul>
+        </Tags>
       </div>
     );
   }
