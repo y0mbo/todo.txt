3f23e2a3a07a6e62882a313c85916939c851d4a8
John Uhri <john.uhri@redbitbluebit.com>
2021-03-31 22:38:26 -0500

    Added IDs to the sample data

--- src/sampleTasks.js
+++ src/sampleTasks.js
@@ -2,31 +2,35 @@
 
 const sampleTasks = {
   task1: {
+    tid: "1",
     priority: "A",
     createDate: "2021-03-12",
     projects: "+banana",
     description: "Create sample data. +banana",
-    raw: "(A) 2021-03-12 Create sample data. +banana",
+    raw: "(A) 2021-03-12 Create sample data. +banana tid:1",
   },
   task2: {
+    tid: "2",
     priority: "B",
     createDate: "2021-03-12",
     projects: "+project",
     description: "Another task with a +project",
-    raw: "(B) 2021-03-12 Another task with a +project",
+    raw: "(B) 2021-03-12 Another task with a +project tid:2",
   },
   task3: {
+    tid: "3",
     contexts: "@context",
     createDate: "2021-03-12",
     description: "Task with @context",
-    raw: "2021-03-12 Task with @context",
+    raw: "2021-03-12 Task with @context tid:3",
   },
   task4: {
+    tid: "a",
     contexts: "@context",
     createDate: "2021-03-12",
     projects: "+banana",
     description: "Task with @context +banana",
-    raw: "2021-03-12 Task with @context +banana",
+    raw: "2021-03-12 Task with @context +banana tid:a",
   },
 };
 
