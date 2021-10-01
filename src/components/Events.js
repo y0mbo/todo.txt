988e0088fb259443b831832a1e52a72bf8cf1121
John Uhri <john.uhri@redbitbluebit.com>
2021-09-30 22:26:38 -0500

    Added EventList and iterate through to create individual events.

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,10 +1,23 @@
 import React from "react";
 import css from "styled-components";
 
+const EventList = css.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`;
+
 class Events extends React.Component {
 render(){
     return(
-        <div><h2>Events go here.</h2></div>
+        <div><h2>Events go here.</h2>
+        <EventList>
+            {/* loop through state.events' keys and map that */}
+            {Object.keys(this.props.events).map((key) => (
+                <Event key={key} id={key} details={this.props.events[key]} />
+            ))}
+        </EventList>
+        </div>
     );
 }
 
