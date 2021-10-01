8cc389f20112ceaf121cda042f0c9c586dada414
John Uhri <john.uhri@redbitbluebit.com>
2021-09-30 22:26:04 -0500

    Added single event class.

---
+++ src/components/Event.js
@@ -0,0 +1,14 @@
+import React from "react";
+import css from "styled-components";
+
+class Event extends React.Component {
+    render () {
+
+        return (
+            <li>An event</li>
+        )
+
+    }
+}
+
+export default Event;
