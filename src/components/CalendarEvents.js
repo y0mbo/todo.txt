373e065ab002dfba039306f8f8b16b3c98e5aeb6
John Uhri <john.uhri@redbitbluebit.com>
2021-10-07 22:19:16 -0500

    Adding a trial run of a calendar grid.

--- src/components/CalendarEvents.js
+++ src/components/CalendarEvents.js
@@ -8,17 +8,26 @@ const EventList = css.ul`
     padding: 0;
 `;
 
+
+
 class CalendarEvents extends React.Component {
     render() {
         return (
             <div>
                 <h2>Events go here.</h2>
+            
                 <EventList>
                     {/* loop through state.events' keys and map that */}
                     {Object.keys(this.props.calendarEvents).map((key) => (
                         <CalendarEvent key={key} id={key} details={this.props.calendarEvents[key]} />
                     ))}
                 </EventList>
+                <h2>Test calendar</h2>
+                <div class="calContainer">
+                    <div class="item item-8am">8</div>
+                    <div class="item item-9am">9</div>
+                    <div class="item item-10am">10</div>
+                </div>
             </div>
         );
     }
