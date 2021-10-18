531a981804c5e9fc26bf3bd9d511b9892382e7e5
John Uhri <john.uhri@redbitbluebit.com>
2021-10-17 20:30:41 -0500

    Made the start and end times for events dynamic.

--- src/components/CalendarEvent.js
+++ src/components/CalendarEvent.js
@@ -1,25 +1,42 @@
 import React from "react";
 import css from "styled-components";
 
-const EventStyle = css.div`
-
-    font-size: 1rem;
-    margin-bottom: 10px;
-`;
-
-const Summary = css.div``;
-
 class CalendarEvent extends React.Component {
     render () {
-        const {summary, location, description} = this.props.details;
+        const {duid, summary, location, description, contexts, dtstart, dtend} = this.props.details;
+
+        // for now not edited since only one day is shown at a time.
+        var startCol = "2";
+        var endCol = "3";
+
+        // need to get the date; should be filtered to the dates on the screen.
+        // given a time (e.g. 1315)
+        // take the hour part (e.g. 13) and multiply it by 4 (e.g. 52) and add 1 (e.g. 53)
+        // and that's the grid offset.
+        // take the minute part (e.g. 15) and divide it by 15 (e.g. 1) and that's the offset
+        // add that to the grid offset already computed
+
+        var startDate = new Date(dtstart);
+        var endDate = new Date(dtend);
+
+        var startHour = startDate.getHours();   
+        var startMinute = startDate.getMinutes();
+        var endHour = endDate.getHours();
+        var endMinute = endDate.getMinutes();
+
+        var eventStart = ((startHour * 4)) + (startMinute / 15) + 1;
+        var eventEnd = ((endHour * 4)) + (endMinute / 15) + 1;
+
+        var eventGridArea = eventStart + " / " + startCol + "  / " + eventEnd + " / " + endCol;
+
+        var totallyADuid = "duid" & duid;
 
         return (
-            <EventStyle>
-
-                {summary && <Summary>{summary}</Summary>}
-                {location}
-                {description}
-            </EventStyle>
+            <div className="event d14" id={totallyADuid} style={{gridArea: eventGridArea}}>
+                <div className="title">{summary}</div>
+                <div className="location">{location}</div>
+                <div className="context">{contexts}</div>
+            </div>
         )
 
     }
