8f66d23691bebddcad1dc18d5f4c3bda608a282f
John Uhri <john.uhri@redbitbluebit.com>
2021-10-19 19:15:41 -0500

    Removed styled component from Tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,32 +1,16 @@
 import React from "react";
 import css from "styled-components";
 
-const TaskStyle = css.div`
-  font-size: 1em;
-  margin-bottom: 10px;
-`;
-const Priority = css.span`
-  padding-right: 10px;
-`;
-const CreateDate = css.span`
-  padding-right: 10px;
-`;
-const Raw = css.div`
-  font-size: 0.8rem;
-  color: #999;
-`;
-
 class Task extends React.Component {
   render() {
     const { priority, createDate, description, raw } = this.props.details;
 
     return (
-      <TaskStyle>
-        {priority && <Priority>({priority})</Priority>}
-        {createDate && <CreateDate>{createDate}</CreateDate>}
+      <div className='task'>
+        {priority && <span className={'priority priority' + priority}>({priority}) </span>}
+        {/* {createDate && <CreateDate>{createDate}</CreateDate>} */}
         {description}
-        <Raw>{raw}</Raw>
-      </TaskStyle>
+      </div>
     );
   }
 }
