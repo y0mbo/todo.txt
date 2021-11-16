0f5f94db9cf5976f60a02d562dc0ddc70e163fa2
John Uhri <john.uhri@redbitbluebit.com>
2021-11-15 21:45:02 -0600

    Added contexts to the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -15,7 +15,7 @@ const Modal = ({body, close, show, title, selectedEvent}) => {
                     <main className="modal_content">
                         {body}
                         <div>{selectedEvent ? selectedEvent.location : null}</div>
-                        <div></div>
+                        <div>{selectedEvent.contexts ? selectedEvent.contexts : null}</div>
                     </main>
                     <footer className="modal_footer">
                         <button className="modalClose" onClick={close}>Cancel</button>
