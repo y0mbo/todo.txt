8bd42146595381bc48edc23751f5205353b784a8
John Uhri <john.uhri@redbitbluebit.com>
2022-04-09 11:51:10 -0500

    changed sample task ID to number

--- src/sampleTasks.js
+++ src/sampleTasks.js
@@ -2,7 +2,7 @@
 
 const sampleTasks = {
   task1: {
-    tid: "1",
+    tid: 1,
     priority: "A",
     createDate: "2021-03-12",
     projects: "+banana",
@@ -10,7 +10,7 @@ const sampleTasks = {
     raw: "(A) 2021-03-12 Create sample data. +banana tid:1",
   },
   task2: {
-    tid: "2",
+    tid: 2,
     priority: "B",
     createDate: "2021-03-12",
     projects: "+project",
@@ -18,14 +18,14 @@ const sampleTasks = {
     raw: "(B) 2021-03-12 Another task with a +project tid:2",
   },
   task3: {
-    tid: "3",
+    tid: 3,
     contexts: "@context",
     createDate: "2021-03-12",
     description: "Task with @context",
     raw: "2021-03-12 Task with @context tid:3",
   },
   task4: {
-    tid: "4",
+    tid: 4,
     contexts: "@context",
     createDate: "2021-03-12",
     projects: "+banana",
@@ -34,9 +34,10 @@ const sampleTasks = {
       "2021-03-12 Task with a @context inside the sentence +banana split. tid:a",
   },
   task5: {
-    tid: "5",
+    tid: 5,
     contexts: "@work",
     createDate: "2021-03-12",
+    dueDate: "2021-03-12",
     projects: "+newProject",
     description: "New Project requirements document",
     raw:
